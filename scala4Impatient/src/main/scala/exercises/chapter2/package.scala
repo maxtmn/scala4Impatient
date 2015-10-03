88f3f514a6fed664ec8b1c3e2166b40231217fb6
maxtmn <mastodont1984@gmail.com>
2015-10-03 14:44:10 +0300

    add exercises

--- scala4Impatient/src/main/scala/exercises/chapter2/package.scala
+++ scala4Impatient/src/main/scala/exercises/chapter2/package.scala
@@ -20,6 +20,75 @@ package object chapter2 {
    * EX_2_02.
    * What is the value of an empty block {}? What is its type?
    */
-  def emptyBlock(): Unit = {}
+  def blank(): Unit = {}
+
+  /**
+   * EX_2_03.
+   * Come up with one situation where assignment x = y = 1 is valid
+   */
+  def assignmentValid() {
+    var y: Int = 10
+    var z: Int = 1
+    val x: Unit = y = z
+  }
+
+  /**
+   * EX_2_04.
+   *  Write a Scala equivalent for the Java loop
+   * for (int i = 10; i >= 0; i--) System.out.println(i);
+   */
+  def forLoop(i: Int, print: Boolean) {
+    var count = i
+    while (count >= 0) {
+      if (print) println(count)
+      count = count - 1
+    }
+  }
+
+  /**
+   * EX_2_04.
+   *  Write a Scala equivalent for the Java loop
+   * for (int i = 10; i >= 0; i--) System.out.println(i);
+   */
+  def forLoop_2(i: Int, print: Boolean) {
+    for (n <- i to (0, -1)) { if (print) println(n) }
+  }
+
+  /**
+   * EX_2_05.
+   * Write a procedure countdown(n: Int) that prints the numbers from n to 0
+   */
+  def countDown(i: Int, print: Boolean) {
+    forLoop_2(i, print)
+  }
+
+  /**
+   * EX_2_06.
+   *  Write a for loop for computing the product of the Unicode codes of all letters
+   * in a string. For example, the product of the characters in "Hello" is 9415087488.
+   */
+  def stringUnicodeProduct(str: String): BigInt = {
+    unicodeProductRecursive(str)
+  }
+
+  private def unicodeProductRecursive(str: String, product: BigInt = 1): BigInt = {
+    if (str.length() == 0) { product }
+    else {
+      unicodeProductRecursive(str.tail, product * str.head)
+    }
+
+  }
+
+  /**
+   * EX_2_06.
+   *  Write a for loop for computing the product of the Unicode codes of all letters
+   * in a string. For example, the product of the characters in "Hello" is 9415087488.
+   */
+  def stringUnicodeProduct_2(str: String): BigInt = {
+    //var product = 1 error, implicit type Int is too small
+    var product: BigInt = 1
+    for (c <- str) product = product * c
+    product
+  }
 
 }
--- scala4Impatient/src/test/scala/exercises/chapter2/Tests.scala
+++ scala4Impatient/src/test/scala/exercises/chapter2/Tests.scala
@@ -24,7 +24,7 @@ class Tests {
     assertEquals(" the signum of a number is 0 if the number is 0", 0, signTest(0))
   }
 
-  val empty = exercises.chapter2.emptyBlock()
+  val empty = exercises.chapter2.blank()
 
   @Test def emptyBlock$type() {
     assertTrue("scala empty block {} type is Unit", empty.isInstanceOf[Unit])
@@ -33,4 +33,35 @@ class Tests {
   @Test def emptyBlock$value() {
     assertEquals("empty block {} value is () ", "()", empty.toString)
   }
+
+  @Test def validAssignment() {
+    exercises.chapter2.assignmentValid()
+    assertTrue("valid assignment ", true)
+  }
+
+  @Test def forLoop() {
+    exercises.chapter2.forLoop(10, false)
+    assertTrue("valid assignment ", true)
+  }
+
+  @Test def forLoop$2() {
+    exercises.chapter2.forLoop_2(5, false)
+    assertTrue("valid assignment ", true)
+  }
+
+  @Test def countDown() {
+    exercises.chapter2.countDown(10, false)
+    assertTrue("valid assignment ", true)
+  }
+
+  @Test def stringUnicodeProduct$recursive() {
+    assertEquals("string 'Hello' unicode product is 9415087488", BigInt("9415087488"), exercises.chapter2.stringUnicodeProduct("Hello"))
+
+  }
+
+  @Test def stringUnicodeProduct$loop() {
+    assertEquals("string 'Hello' unicode product is 9415087488", BigInt("9415087488"), exercises.chapter2.stringUnicodeProduct_2("Hello"))
+
+  }
+
 }
