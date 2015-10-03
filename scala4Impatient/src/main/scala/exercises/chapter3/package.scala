d17bed1f3ca1b94433dc216c6f86c4a3add2182e
maxtmn <mastodont1984@gmail.com>
2015-10-04 00:36:12 +0300

    add tasks

--- scala4Impatient/src/main/scala/exercises/chapter3/package.scala
+++ scala4Impatient/src/main/scala/exercises/chapter3/package.scala
@@ -21,4 +21,57 @@ package object chapter3 {
    * Write a loop that swaps adjacent elements of an array of integers. For example,
    * Array(1, 2, 3, 4, 5) becomes Array(2, 1, 4, 3, 5).
    */
+
+  /**
+   *  EX_3_03.
+   *  Repeat the preceding assignment, but produce a new array with the swapped
+   *  values. Use for/yield.
+   */
+
+  /**
+   *  EX_3_04.
+   *  Given an array of integers, produce a new array that contains all positive
+   *  values of the original array, in their original order, followed by all values that
+   *  are zero or negative, in their original order.
+   */
+
+  /**
+   * EX_3_05.
+   *  How do you compute the average of an Array[Double]?
+   */
+
+  /**
+   *  EX_3_06.
+   *   How do you rearrange the elements of an Array[Int] so that they appear in
+   *  reverse sorted order? How do you do the same with an ArrayBuffer[Int]?
+   */
+
+  /**
+   *  EX_3_07.
+   *   Write a code snippet that produces all values from an array with duplicates
+   *  removed. (Hint: Look at Scaladoc.)
+   */
+
+  /**
+   * EX_3_08.
+   * Rewrite the example at the end of Section 3.4, "Transforming Arrays," on
+   *  page 34 using the drop method for dropping the index of the first match. Look
+   *  the method up in Scaladoc.
+   */
+
+  /**
+   *  EX_3_09.
+   *  Make a collection of all time zones returned by java.util.TimeZone.getAvailableIDs
+   *  that are in America. Strip off the "America/" prefix and sort the result.
+   */
+
+  /**
+   * EX_3_10.
+   *  Import java.awt.datatransfer._ and make an object of type SystemFlavorMap with
+   * the call
+   *  val flavors = SystemFlavorMap.getDefaultFlavorMap().asInstanceOf[SystemFlavorMap]
+   *  Then call the getNativesForFlavor method with parameter DataFlavor.imageFlavor
+   *  and get the return value as a Scala buffer. (Why this obscure class? It's hard
+   *  to find uses of java.util.List in the standard Java library.)
+   */
 }
