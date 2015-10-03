4cab43804e098a8f28cc518249342eb8626933ff
maxtmn <mastodont1984@gmail.com>
2015-10-03 12:07:20 +0300

    add package for chapter 2 exercises

---
+++ scala4Impatient/src/main/scala/exercises/chapter2/package.scala
@@ -0,0 +1,15 @@
+package exercises
+
+/**
+ * Control structures and functions
+ * @author Tereshchenko
+ */
+package object chapter2 {
+  /**
+   * EX_2_01.
+   * the signum of a number is 1 if the number is positive,
+   *  -1 if it is negative and 0 if it is zero.
+   *  Write function that computes this value
+   */
+
+}
