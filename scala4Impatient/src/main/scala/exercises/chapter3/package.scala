b41da5fdd3f1c1c1dd81298582fb4815080f80a6
maxtmn <mastodont1984@gmail.com>
2015-10-04 23:42:41 +0300

    add exercises

--- scala4Impatient/src/main/scala/exercises/chapter3/package.scala
+++ scala4Impatient/src/main/scala/exercises/chapter3/package.scala
@@ -21,12 +21,26 @@ package object chapter3 {
    * Write a loop that swaps adjacent elements of an array of integers. For example,
    * Array(1, 2, 3, 4, 5) becomes Array(2, 1, 4, 3, 5).
    */
+  def swapAdjacentElements(array: Array[Int]): Array[Int] = {
+    for (i <- 0 until array.length if (i % 2 == 1)) {
+      var tmp = array(i)
+      array(i) = array(i - 1)
+      array(i - 1) = tmp
+    }
+    array
+  }
 
   /**
    *  EX_3_03.
    *  Repeat the preceding assignment, but produce a new array with the swapped
    *  values. Use for/yield.
    */
+  def swapAdjacentElementsYield(array: Array[Int]) = {
+    for (i <- 0 until array.length) yield if (i % 2 == 1) array(i - 1) else {
+      if (i == array.length - 1) array(i)
+      else array(i + 1)
+    }
+  }
 
   /**
    *  EX_3_04.
@@ -34,23 +48,49 @@ package object chapter3 {
    *  values of the original array, in their original order, followed by all values that
    *  are zero or negative, in their original order.
    */
-
+  def swapArrayIntegers(array: Array[Int]) = {
+    array.filter(_ > 0) ++ array.filter(_ == 0) ++ array.filter(_ < 0)
+  }
   /**
    * EX_3_05.
    *  How do you compute the average of an Array[Double]?
    */
+  def arrayAvarage(array: Array[Double]): Double = {
+    array.foldLeft(double2Double(1.0))((a, b) => a + b) / array.length
+  }
+  def arrayAvarageSum(array: Array[Double]): Double = {
+    array.sum / array.length
+  }
 
   /**
    *  EX_3_06.
    *   How do you rearrange the elements of an Array[Int] so that they appear in
    *  reverse sorted order? How do you do the same with an ArrayBuffer[Int]?
    */
+  def reverseSortOrder(array: Array[Int]) = {
+    array.sorted.reverse
+  }
+
+  def reverseSortOrderWith(array: Array[Int]) = {
+    array.sortWith(_ > _)
+  }
+
+  def reverseSortOrderBuffer(array: ArrayBuffer[Int]) = {
+    array.sorted.reverse
+  }
+
+  def reverseSortOrderBufferWith(array: ArrayBuffer[Int]) = {
+    array.sortWith(_ > _)
+  }
 
   /**
    *  EX_3_07.
    *   Write a code snippet that produces all values from an array with duplicates
    *  removed. (Hint: Look at Scaladoc.)
    */
+  def arrayDistinct(array: Array[Int]) = {
+    array.distinct.toArray
+  }
 
   /**
    * EX_3_08.
--- scala4Impatient/src/test/scala/exercises/chapter3/Tests.scala
+++ scala4Impatient/src/test/scala/exercises/chapter3/Tests.scala
@@ -1,5 +1,7 @@
 package exercises.chapter3
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
  * @author Tereshchenko
  */
@@ -13,6 +15,71 @@ class Tests {
   @Test def fillArray() {
     populate(10)
     assertEquals("array filled with 10 elements", 10, a.length)
+  }
+
+  @Test def swapArrayTest() {
+    val exp = Array(2, 1, 4, 3, 5)
+    val res = swapAdjacentElements(Array(1, 2, 3, 4, 5))
+    for (i <- 0 until exp.length) {
+      assertEquals("check swap order", exp(i), res(i))
+    }
+    assertArrayEquals(exp, res);
+  }
+
+  @Test def swapArrayYieldTest() {
+    val exp = Array[Int](2, 1, 4, 3, 5)
+    val res = swapAdjacentElementsYield(Array(1, 2, 3, 4, 5)).toArray
+    for (i <- 0 until exp.length) {
+      assertEquals("check swap order", exp(i), res(i))
+    }
+    assertArrayEquals(exp, res);
+  }
+
+  @Test def swapArrayIntegersTest() {
+    val exp = Array(2, 1, 4, -3, -5)
+    val res = swapArrayIntegers(Array(2, -3, 1, 4, -5))
+    for (i <- 0 until exp.length) {
+      assertEquals("check swap order", exp(i), res(i))
+    }
+    assertArrayEquals(exp, res);
+  }
+
+  @Test def arrayAvarageTest() {
+    assertEquals(5.5, arrayAvarage(Array[Double](1, 2, 3, 4, 5, 6, 7, 8, 9, 10)), 0.1)
+  }
+
+  @Test def arrayAvarageSumTest() {
+    assertEquals(5.5, arrayAvarageSum(Array[Double](1, 2, 3, 4, 5, 6, 7, 8, 9, 10)), 0.001)
+  }
+
+  @Test def reverseSortOrderTest() {
+    val exp = Array(4, 2, 1, -3, -5)
+    val res = reverseSortOrder(Array(2, -3, 1, 4, -5))
+    assertArrayEquals(exp, res);
+  }
+
+  @Test def reverseSortOrderWithTest() {
+    val exp = Array(4, 2, 1, -3, -5)
+    val res = reverseSortOrderWith(Array(2, -3, 1, 4, -5))
+    assertArrayEquals(exp, res);
+  }
+
+  @Test def reverseSortOrderBufferTest() {
+    val exp = Array(4, 2, 1, -3, -5)
+    val res = reverseSortOrderBuffer(ArrayBuffer(2, -3, 1, 4, -5))
+    assertArrayEquals(exp, res.toArray);
+  }
+
+  @Test def reverseSortOrderBufferWithTest() {
+    val exp = Array(4, 2, 1, -3, -5)
+    val res = reverseSortOrderBufferWith(ArrayBuffer(2, -3, 1, 4, -5))
+    assertArrayEquals(exp, res.toArray);
+  }
+
+  @Test def arrayDistinctTest() {
+    val exp = Array(2, -3, 1, 4, -5)
+    val res = arrayDistinct(Array(2, -3, 1, 2, 4, -3, -5))
+    assertArrayEquals(exp, res);
   }
 
 }
